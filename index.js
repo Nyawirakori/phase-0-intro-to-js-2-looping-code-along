d23a276590891ff8d52433dbea10739d7e128395
Joan Nyawira Kori <korijoan171@gmail.com>
2025-02-26 21:22:06 +0300

    Modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+ 
+function writeCards(cards, event){
+    let messages=[];/* The empty array that will hold the messages*/
+    for (let i=0; i < cards.length; i++){
+        messages.push (`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    }
+     return messages;
+}
+/*while loop*/
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
